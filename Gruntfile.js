1e83ffeae6d392f876d6191216a97a3a5304ed3f
jimmynicol <james@fundly.com>
2013-08-29 10:13:29 -0700

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = function(grunt){
+
+  require('time-grunt')(grunt);
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      all: ['Gruntfile.js', 'lib/{,*/}*.js', 'test/{,*/}*.js']
+    },
+    simplemocha: {
+      options: {
+        timeout: 3000,
+        ignoreleaks: false,
+        reporter: 'spec',
+        ui: 'bdd'
+      },
+      all: {
+        src: ['test/**/*.js']
+      }
+    },
+    watch: {
+      test: {
+        files: ['lib/**/*.js', 'test/**/*.js'],
+        tasks: ['simplemocha']
+      },
+      jshint: {
+        files: ['Gruntfile.js', 'lib/**/*.js', 'test/**/*.js'],
+        tasks: ['jshint']
+      }
+    }
+  });
+
+  grunt.registerTask('test',    ['simplemocha']);
+  grunt.registerTask('default', ['jshint', 'test', 'watch']);
+
+};
