a61b99af58cd656f623d520d6cd76e8aae3f43ef
James Nicol <james.andrew.nicol@gmail.com>
2014-05-18 22:26:45 -0700

    Refactoring the cli script to include filters

--- bin/image_resizer.js
+++ bin/image_resizer.js
@@ -3,7 +3,7 @@
 'use strict';
 
 var program, path, fs, mkdirp, pkg, chalk,
-    dest, appName, newPkg;
+    dest, appName, action, newPkg;
 
 
 program = require('commander');
@@ -14,60 +14,9 @@ chalk   = require('chalk');
 pkg     = require('../package.json');
 
 
-program
-  .version(pkg.version)
-  .usage('[action] [options] [dir]')
-  .parse(process.argv);
-
-// dest = program.args.shift() || '.';
-dest = '.';
-appName = path.basename(path.resolve(dest));
-
-// create a new package.json
-newPkg = {
-  name: appName,
-  version: '0.0.1',
-  main: 'index.js',
-  engines: {
-    'node': pkg.engines.node
-  },
-  dependencies: {
-    'image-resizer': '~' + pkg.version,
-    'express': pkg.dependencies.express,
-    'lodash': pkg.dependencies.lodash
-  },
-  devDependencies: pkg.devDependencies
-};
-write(dest + '/package.json', JSON.stringify(newPkg, null, 2));
-
-// create index.js
-copy(__dirname + '/./templates/index.js.tmpl', dest + '/index.js');
-
-// create the gulpfile
-copy(__dirname + '/./templates/gulpfile.js.tmpl', dest + '/gulpfile.js');
-
-// create .env
-copy(__dirname + '/./templates/.env.tmpl', dest + '/.env');
-
-// create .gitignore
-copy(__dirname + '/./templates/.gitignore.tmpl', dest + '/.gitignore');
-
-// create .jshintrc
-copy(__dirname + '/../.jshintrc', dest + '/.jshintrc');
-
-// create Heroku files
-//  - Procfile
-//  - .buildpacks
-write (dest + '/Procfile', 'web: node index.js');
-copy(__dirname + '/../.buildpacks', dest + '/.buildpacks');
-
-// create plugin folders
-//  - sources
-//  - filters
-mkdir(dest + '/plugins/sources');
-mkdir(dest + '/plugins/filters');
-
-
+/**
+File/Directory helper functions
+*/
 function write(path, str, mode) {
   fs.writeFile(path, str, { mode: mode || '0666' });
   console.log('    ' + chalk.green('create') + ': ' + path);
@@ -83,6 +32,95 @@ function mkdir(path, fn) {
       throw err;
     }
     console.log('    ' + chalk.green('create') + ' : ' + path);
-    fn && fn();
+    if (typeof fn === 'function'){
+      fn();
+    }
   });
+}
+
+
+function createApplicationAt(path){
+  console.log('\n' + chalk.cyan('Creating new ') + chalk.cyan.bold('image-resizer') + chalk.cyan(' app!'));
+  console.log();
+
+  process.on('exit', function(){
+    console.log();
+    console.log(chalk.green('   now install your dependencies') + ':');
+    console.log('     $ npm install');
+    console.log();
+    console.log(chalk.green('   run the app') + ':');
+    console.log('     $ gulp watch');
+    console.log();
+  });
+
+  // create a new package.json
+  newPkg = {
+    name: appName,
+    version: '0.0.1',
+    main: 'index.js',
+    engines: {
+      'node': pkg.engines.node
+    },
+    dependencies: {
+      'image-resizer': '~' + pkg.version,
+      'express': pkg.dependencies.express,
+      'lodash': pkg.dependencies.lodash
+    },
+    devDependencies: pkg.devDependencies
+  };
+
+  write(path + '/package.json', JSON.stringify(newPkg, null, 2));
+
+  // create index.js
+  copy(__dirname + '/./templates/index.js.tmpl', path + '/index.js');
+
+  // create the gulpfile
+  copy(__dirname + '/./templates/gulpfile.js.tmpl', path + '/gulpfile.js');
+
+  // create .env
+  copy(__dirname + '/./templates/.env.tmpl', path + '/.env');
+
+  // create .gitignore
+  copy(__dirname + '/./templates/.gitignore.tmpl', path + '/.gitignore');
+
+  // create .jshintrc
+  copy(__dirname + '/../.jshintrc', path + '/.jshintrc');
+
+  // create Heroku files
+  //  - Procfile
+  //  - .buildpacks
+  write (path + '/Procfile', 'web: node index.js');
+  copy(__dirname + '/../.buildpacks', path + '/.buildpacks');
+
+  // create plugin folders
+  //  - sources
+  //  - filters
+  mkdir(path + '/plugins/sources');
+  mkdir(path + '/plugins/filters');
+}
+
+/**
+Create the program and list the possible commands
+*/
+program
+  .version(pkg.version)
+  .usage('[action] [options]')
+  .command('new', 'Create new clean image-resizer app')
+  .command('filter [name]', 'Create new filter stream')
+  .option('-f, --force', 'force on non-empty directory')
+  .parse(process.argv);
+
+dest = '.';
+appName = path.basename(path.resolve(dest));
+action = program.args[0];
+
+switch(action){
+case 'new':
+  createApplicationAt(dest);
+  break;
+
+case 'filter':
+  var filterName = program.args[1];
+  copy(__dirname + '/./templates/filter.js.tmpl', dest + '/plugins/filters/' + filterName + '.js');
+  break;
 }
