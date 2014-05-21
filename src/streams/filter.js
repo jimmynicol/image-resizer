703ab4b5459387ecd163f3ef09fcbe552d7541d7
James Nicol <james.andrew.nicol@gmail.com>
2014-05-20 22:38:05 -0700

    Fixed issue with filter streams, added filter ex.

--- src/streams/filter.js
+++ src/streams/filter.js
@@ -7,41 +7,45 @@ filters = require('./filters');
 _       = require('lodash');
 
 
-module.exports = map(function(image, callback){
+module.exports = function(){
 
-  // pass through if there is an error
-  if (image.isError()){
-    return callback(null, image);
-  }
+  return map(function(image, callback){
 
-  // let this pass through if we are requesting the metadata as JSON
-  if (image.modifiers.action === 'json'){
-    image.log.log('filter: json metadata call');
-    return callback(null, image);
-  }
-
-  var filter = image.modifiers.filter;
-
-  // don't attempt to process a filter if no appropriate modifier is set
-  if (typeof filter === 'undefined'){
-    image.log.log('filter:', image.log.colors.bold('none requested'));
-    return callback(null, image);
-  }
-
-  image.log.time('filter:'+ filter);
-
-  // run the appropriate filter
-  filters[image.modifiers.filter](image, function(err, data){
-    image.log.timeEnd('filter:'+ filter);
-
-    if (err) {
-      image.log.error('filter error', err);
-      image.error = new Error(err);
-    } else {
-      image.contents = data;
+    // pass through if there is an error
+    if (image.isError()){
+      return callback(null, image);
     }
 
-    callback(null, image);
+    // let this pass through if we are requesting the metadata as JSON
+    if (image.modifiers.action === 'json'){
+      image.log.log('filter: json metadata call');
+      return callback(null, image);
+    }
+
+    var filter = image.modifiers.filter;
+
+    // don't attempt to process a filter if no appropriate modifier is set
+    if (typeof filter === 'undefined'){
+      image.log.log('filter:', image.log.colors.bold('none requested'));
+      return callback(null, image);
+    }
+
+    image.log.time('filter:'+ filter);
+
+    // run the appropriate filter
+    filters[image.modifiers.filter](image, function(err, data){
+      image.log.timeEnd('filter:'+ filter);
+
+      if (err) {
+        image.log.error('filter error', err);
+        image.error = new Error(err);
+      } else {
+        image.contents = data;
+      }
+
+      callback(null, image);
+    });
+
   });
 
-});
+};
